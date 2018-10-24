46581c526382ca905c80ae53ca0e7bb663a63eb6
Radames Ajna <radamajna@gmail.com>
2018-10-23 17:33:05 -0700

    adding composite projection, albers and USA territories, d3 transitions

--- src/util/d3.js
+++ src/util/d3.js
@@ -1,10 +1,14 @@
 import * as selection from "d3-selection";
+import * as transition from "d3-transition";
+import * as ease from "d3-ease";
 import * as geo from "d3-geo";
 import completeAssign from "./complete-assign";
 
 const d3 = completeAssign({},
   selection,
+  transition,
   geo,
+  ease,
 );
 
 export default d3;
